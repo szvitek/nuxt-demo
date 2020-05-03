0747df7d91a7b5bb673ba31bb18c4b84a19b940c
szvitek <gy.szvitek@gmail.com>
2020-05-04 00:40:59 +0100

    feat(posts): pages, layouts, components, assets

---
+++ pages/about/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="about-page">
+    <h1>About page</h1>
+    <p>lorem ipsum dolor sit amet</p>
+    <p>lorem ipsum dolor sit amet</p>
+    <p>lorem ipsum dolor sit amet</p>
+    <p>lorem ipsum dolor sit amet</p>
+  </div>
+</template>
+
+<style scoped>
+.about-page {
+  text-align: center;
+  padding: 30px;
+}
+</style>
---
+++ pages/posts/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="post-page">
+    POST PAGE
+  </div>
+</template>
+
+<style scoped>
+.post-page {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
